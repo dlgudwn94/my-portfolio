bed1f444ddbfc603772571229a2120547037522b
Hyungju <dlgudwn94>
2025-07-11 22:33:30 +0900

    [Modify]: project data 수정

--- my-portfolio/src/data/projects.ts
+++ my-portfolio/src/data/projects.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Tairo",
-    description: "타로를 뽑고 해석을 볼 수 있는 웹 앱입니다. 타로 분위기를 살린 UI와 Supabase 기반 백엔드를 사용한 팀프로젝트입니다.",
+    description: "타로를 뽑고 해석을 볼 수 있는 웹 앱입니다. 타로 분위기를 살린 UI와 Supabase 기반 백엔드를 활용한 팀 프로젝트입니다.\n 저는 타로 기록 조회 기능을 담당했습니다. 캘린더 라이브러리를 활용해 만든 컴포넌트를 통해 사용자의 과거 타로 결과를 날짜별로 시각화하고, 특정 날짜의 결과를 조회할 수 있도록 구현했습니다. 또한 Supabase를 사용해 사용자 인증과 데이터 저장을 처리했습니다.",
     tech: ["React", "TypeScript", "TailwindCSS", "Supabase"],
     github: "https://github.com/FRONTENDBOOTCAMP-12th/Hard-working-6th",
     live: "https://tairo.vercel.app/",
