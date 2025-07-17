40a363433547e47e763b6094f6f36d3d0c55551c
dlgudwn94 <dlgudwn82@naver.com>
2025-07-17 16:41:31 +0900

    [Fix]: 기본 메일 앱이 등록 되어있지 않은 경우 빈 창이 뜨는 이슈 해결

--- my-portfolio/src/sections/Contact.tsx
+++ my-portfolio/src/sections/Contact.tsx
@@ -1,11 +1,12 @@
-import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { SiGmail } from "react-icons/si";
 
 function Contact() {
   return (
     <section id="contact" className="w-full px-6">
       <div className="flex justify-start gap-6 mt-4">
-        <a href="mailto:dlgudwn3739@gmail.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-teal-400 transition-colors" aria-label="Send Email">
-          <AiOutlineMail className="w-6 h-6" />
+        <a href="https://mail.google.com/mail/?view=cm&to=dlgudwn3739@gmail.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-teal-400 transition-colors" aria-label="Send Email via Gmail">
+          <SiGmail className="w-6 h-6" />
         </a>
 
         <a href="https://github.com/dlgudwn94" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-teal-400 transition-colors" aria-label="GitHub Profile">
